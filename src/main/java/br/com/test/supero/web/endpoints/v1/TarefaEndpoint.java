80e975fafb6bc655c8a1e9b5db6abb1e2dfe0606
Michel Miola <miola.michel@gmail.com>
2021-06-06 10:45:00 -0300

    ajuste path

--- src/main/java/br/com/test/supero/web/endpoints/v1/TarefaEndpoint.java
+++ src/main/java/br/com/test/supero/web/endpoints/v1/TarefaEndpoint.java
@@ -9,7 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/v1/tarefa")
+@RequestMapping("/v1/tarefas")
 public class TarefaEndpoint {
 
     private TarefaService tarefaService;
